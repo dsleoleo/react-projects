0c3851940982fb479e1cf4fff198f5fb8bc4178f
dsleoleo <leozhou@Leos-MacBook-Pro.local>
2017-06-17 01:33:53 -0700

    react components

--- todo-list/src/index.jsx
+++ todo-list/src/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(<div>Hell world</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
