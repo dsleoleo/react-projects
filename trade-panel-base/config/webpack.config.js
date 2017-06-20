95aef7f28369320f4d894613f29a7d0ef96dfc04
dsleoleo <leozhou@Leos-MacBook-Pro.local>
2017-06-20 13:58:11 -0700

    fixed webpack compiling scss

--- trade-panel-base/config/webpack.config.js
+++ trade-panel-base/config/webpack.config.js
@@ -1,19 +1,35 @@
 // import webpack npm module
 
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.scss'],
         modules: ['src', 'node_modules'],
     },
     module: {
-        loaders: [
+        rules: [
+            /*
+            your other rules for JavaScript transpiling go in here
+            */
             {
                 test: /\.jsx?$/,
-                loaders: ['babel-loader'],
-            }
-        ],
+                use: [{ loader: 'babel-loader' }],
+            },
+            {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }
+        ]
     }
     ,
     entry: [
@@ -22,5 +38,11 @@ module.exports = {
     output: {
         filename: 'app.js',
         path: __dirname + '/../dist',
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({ // define where to save the file
+            filename: '[name].bundle.css',
+            allChunks: true,
+        }),
+    ],
 };
--- trade-panel-base/src/index.jsx
+++ trade-panel-base/src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import "./css/variables.scss";
+import "./css/notionalInput.scss";
+import "./css/priceButton.scss";
+import "./css/priceMovement.scss";
+import "./css/spotTile.scss";
+import "./css/tradeNotification.scss";
 
 ReactDOM.render(<App />, document.getElementById('root'));
