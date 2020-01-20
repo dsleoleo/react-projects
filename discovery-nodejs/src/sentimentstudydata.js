890918e3a4188acb9cff4a17a9036b8ba666a2d0
Leo Zhou <leo@exabeam.com>
2020-01-20 11:41:17 -0800

    first chart

--- discovery-nodejs/src/sentimentstudydata.js
+++ discovery-nodejs/src/sentimentstudydata.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-",negative,neu,positive,compound,positive/negative"
-"What was done exceptionally well,0.038,0.597,0.366,0.9993,9.631578947"
-"What could be done better,0.056,0.679,0.266,0.9996,4.75"
-"Would you recommend the product,0.075,0.569,0.356,0.9999,4.746666667"
-"Customer service satisfaction,0.087,0.618,0.295,0.999,3.390804598"
-"Product satisfaction,0.088,0.639,0.273,0.9983,3.102272727"
-"Performance of specific resource,0.09,0.611,0.3,0.9994,3.333333333"
-=======
 export const _dataString_ = ",negative,neu,positive,compound,positive/negative\n" + 
 "What was done exceptionally well,0.038,0.597,0.366,0.9993,9.631578947\n" + 
 "What could be done better,0.056,0.679,0.266,0.9996,4.75\n" + 
@@ -14,4 +5,3 @@ export const _dataString_ = ",negative,neu,positive,compound,positive/negative\n
 "Customer service satisfaction,0.087,0.618,0.295,0.999,3.390804598\n" + 
 "Product satisfaction,0.088,0.639,0.273,0.9983,3.102272727\n" + 
 "Performance of specific resource,0.09,0.611,0.3,0.9994,3.333333333";
->>>>>>> added sentiment data
